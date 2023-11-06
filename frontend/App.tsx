ce41e62ae50018a4d4c023ab5e43ae7985bf1f45
adriancielniak <adriancielniak@gmail.com>
2023-11-06 01:13:52 +0100

    Add Login Screen

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,56 +1,18 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import Tabs from './src/components/Tabs';
+import Login from './src/components/Login';
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-      <NavigationContainer>
-        <Tabs />
-      </NavigationContainer>
+      <Login />
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
 export default App;
+
+/*
+<NavigationContainer>
+        <Tabs />
+</NavigationContainer>
+*/
---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,78 @@
+
+import React from "react"
+import {SafeAreaView, StyleSheet, Text, View} from "react-native";
+import { TextInput, TouchableOpacity } from "react-native";
+
+const Login = () =>{
+    return(
+        <SafeAreaView style={styles.containter}>
+            <Text style={styles.title}> Login Screen</Text>
+
+            <View style={styles.inputView}>
+                <TextInput style={styles.TextInput} placeholder="Enter email address" autoCapitalize="none" />
+            </View>
+
+            <View style={styles.inputView}>
+                <TextInput style={styles.TextInput} placeholder="Enter password" autoCapitalize="none" secureTextEntry={true} />
+            </View>
+
+            <TouchableOpacity>
+                <Text style={styles.forgot_button}>Forgot Password?</Text> 
+            </TouchableOpacity> 
+            <TouchableOpacity style={styles.loginBtn}>
+                <Text style={styles.LoginText}>LOGIN</Text> 
+            </TouchableOpacity>
+        </SafeAreaView>
+    )
+}
+export default Login;
+
+const styles = StyleSheet.create({
+    containter:{
+        flex:1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "white",
+    },
+    title:{
+        fontWeight: "bold",
+        fontSize:50,
+        color: "black",
+        marginBottom: 150,
+    },
+    inputView: {
+        backgroundColor: "black",
+        borderRadius: 30,
+        width: "70%",
+        height: 45,
+        marginBottom: 20,
+        alignItems: "flex-start",
+    },
+      TextInput: {
+        height: 50,
+        flex: 1,
+        padding: 10,
+        marginLeft: 20,
+        color: "white"
+    },
+
+    forgot_button: {
+        height: 30,
+        marginBottom: 30,
+    },
+    LoginText: {
+        height: 50,
+        flex: 1,
+        padding: 12,
+        color: "white"
+    },
+
+    loginBtn: {
+        backgroundColor: "black",
+        borderRadius: 30,
+        width: "50%",
+        height: 45,
+        marginBottom: 20,
+        alignItems: "center",
+      },
+})
