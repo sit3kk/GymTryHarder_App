e4d6eb80a22353e3f4f7c4f3ba2c5ee1d12a68de
adriancielniak <adriancielniak@gmail.com>
2023-12-07 23:29:24 +0100

    Move example  data

---
+++ frontend/src/assets/data.tsx
@@ -0,0 +1,61 @@
+const TrainingSet1 = [
+    { 
+      id: 1,
+      title: 'plecy + barki',
+      name: 'Adrian',
+      surname: 'Nowak Zalno',
+      date: '2023-12-01',
+      imageUri: require('../assets/zalno.jpeg'),
+      exercises: ['bench press', 'one hand seated row', 'squat', 'leg extension', 'incline dumbell press', 'cable fly', ]
+    },
+    { 
+      id: 2,
+      title: 'klata bic ogien',
+      name: 'Adrian',
+      surname: 'Nowak Zalno',
+      date: '2023-12-02',
+      imageUri: require('../assets/zalno.jpeg'),
+      exercises: ['bench press', 'one hand seated row', 'squat']
+    },
+    { 
+      id: 3,
+      title: 'zestaw dyskotekowy',
+      name: 'Adrian',
+      surname: 'Nowak Zalno',
+      date: '2023-12-03',
+      imageUri: require('../assets/zalno.jpeg'),
+      exercises: ['bench press', 'one hand seated row', 'squat']
+    },
+  ];
+
+const TrainingSet2 = [
+    { 
+        id: 4,
+        title: 'plecy + barki',
+        name: 'cbum',
+        surname: 'cbum',
+        date: '2023-12-01',
+        imageUri: require('../assets/cbum.jpeg'),
+        exercises: ['bench press', 'one hand seated row', 'squat']
+      },
+      { 
+        id: 5,
+        title: 'klata bic ogien',
+        name: 'cbum',
+        surname: 'cbum',
+        date: '2023-12-02',
+        imageUri: require('../assets/cbum.jpeg'),
+        exercises: ['bench press', 'one hand seated row', 'squat']
+      },
+      { 
+        id: 6,
+        title: 'zestaw dyskotekowy',
+        name: 'cbum',
+        surname: 'cbum',
+        date: '2023-12-03',
+        imageUri: require('../assets/cbum.jpeg'),
+        exercises: ['bench press', 'one hand seated row', 'squat']
+      },
+]
+
+export { TrainingSet1, TrainingSet2 };
--- frontend/src/screens/Home.tsx
+++ frontend/src/screens/Home.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import { View, Text, TouchableOpacity, ScrollView, SafeAreaView, StyleSheet, Image } from "react-native"
+import { TrainingSet1, TrainingSet2 } from "../assets/data";
 import SingleTraining from "../components/SingleTraining";
 
 const Home = () => {
@@ -8,68 +9,6 @@ const Home = () => {
     const handleButtonPress = (button: string) =>{
         setActiveButton(button);
     }   
-
-    const TrainingSet1 = [
-        { 
-          id: 1,
-          title: 'plecy + barki',
-          name: 'Adrian',
-          surname: 'Nowak Zalno',
-          date: '2023-12-01',
-          imageUri: require('../assets/zalno.jpeg'),
-          exercises: ['bench press', 'one hand seated row', 'squat', 'leg extension', 'incline dumbell press', 'cable fly', ]
-        },
-        { 
-          id: 2,
-          title: 'klata bic ogien',
-          name: 'Adrian',
-          surname: 'Nowak Zalno',
-          date: '2023-12-02',
-          imageUri: require('../assets/zalno.jpeg'),
-          exercises: ['bench press', 'one hand seated row', 'squat']
-        },
-        { 
-          id: 3,
-          title: 'zestaw dyskotekowy',
-          name: 'Adrian',
-          surname: 'Nowak Zalno',
-          date: '2023-12-03',
-          imageUri: require('../assets/zalno.jpeg'),
-          exercises: ['bench press', 'one hand seated row', 'squat']
-        },
-      ];
-
-    const TrainingSet2 = [
-        { 
-            id: 4,
-            title: 'plecy + barki',
-            name: 'cbum',
-            surname: 'cbum',
-            date: '2023-12-01',
-            imageUri: require('../assets/cbum.jpeg'),
-            exercises: ['bench press', 'one hand seated row', 'squat']
-          },
-          { 
-            id: 5,
-            title: 'klata bic ogien',
-            name: 'cbum',
-            surname: 'cbum',
-            date: '2023-12-02',
-            imageUri: require('../assets/cbum.jpeg'),
-            exercises: ['bench press', 'one hand seated row', 'squat']
-          },
-          { 
-            id: 6,
-            title: 'zestaw dyskotekowy',
-            name: 'cbum',
-            surname: 'cbum',
-            date: '2023-12-03',
-            imageUri: require('../assets/cbum.jpeg'),
-            exercises: ['bench press', 'one hand seated row', 'squat']
-          },
-    ]
-
-
 
     const renderTrainings = () =>{
         if(activeButton == "You"){
