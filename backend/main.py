c5441318f523f66d43969b247ba253f3f6453abd
sit3kk <sito.270@interia.pl>
2023-12-30 17:05:14 +0100

    Certificates

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,20 @@ from fastapi import FastAPI
 from routes import router, init_db
 from database import engine, test_connection
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(router)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup_event():
@@ -12,4 +23,4 @@ async def startup_event():
     await test_connection()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=80, ssl_certfile='cert.pem', ssl_keyfile='key.pem')
+    uvicorn.run("main:app", host="0.0.0.0", port=443, ssl_certfile='cert.pem', ssl_keyfile='key.pem')
