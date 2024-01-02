c227fb7a1282864804c619eccd244385ccd35347
adriancielniak <adriancielniak@gmail.com>
2024-01-02 20:54:33 +0100

    Add eas and packages to enable ssl pinning on expo

--- frontend/ios/frontend/AppDelegate.mm
+++ frontend/ios/frontend/AppDelegate.mm
@@ -1,42 +1,20 @@
 #import "AppDelegate.h"
-#import <React/RCTBundleURLProvider.h>
-#import <TrustKit/TrustKit.h>
 
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTLinkingManager.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  //[self initTrustKit];
-
   self.moduleName = @"main";
-  //
-  //
+
+  // You can add your custom initial props in the dictionary below.
+  // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
-
-/*
-- (void)initTrustKit {
-      NSDictionary *trustKitConfig =
-  @{
-    kTSKSwizzleNetworkDelegates: @YES,                    
-    kTSKPinnedDomains : @{
-            @"https://3.121.219.180" : @{
-              kTSKIncludeSubdomains: @YES,
-              kTSKEnforcePinning : @YES,
-              //kTSKDisableDefaultReportUri: @YES
-                    kTSKPublicKeyHashes : @[
-                        @"Upmz83F2acKWXA+7W9WVxADNxAvhiAobLZhoQvnBEsI=",
-                        @"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="
-                            ],
-              //kTSKPublicKeyAlgorithms : @[kTSKAlgorithmRsa2048],
-                    },
-            }};
-    [TrustKit initSharedInstanceWithConfiguration:trustKitConfig];
-}
-*/
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
@@ -46,5 +24,34 @@
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
-@end
 
+// Linking API
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+  return [super application:application openURL:url options:options] || [RCTLinkingManager application:application openURL:url options:options];
+}
+
+// Universal Links
+- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler {
+  BOOL result = [RCTLinkingManager application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
+  return [super application:application continueUserActivity:userActivity restorationHandler:restorationHandler] || result;
+}
+
+// Explicitly define remote notification delegates to ensure compatibility with some third-party libraries
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+  return [super application:application didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+}
+
+// Explicitly define remote notification delegates to ensure compatibility with some third-party libraries
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+  return [super application:application didFailToRegisterForRemoteNotificationsWithError:error];
+}
+
+// Explicitly define remote notification delegates to ensure compatibility with some third-party libraries
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
+{
+  return [super application:application didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
+}
+
+@end
