5ae689630f2f20ec7541fe2f920165b8d1cb9072
adriancielniak <adriancielniak@gmail.com>
2024-01-03 16:08:16 +0100

    Switch endpoint adress

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -11,7 +11,7 @@ interface AuthProps {
 }
 
 const TOKEN_KEY = 'my-jwt';
-export const API_URL = 'http://127.0.0.1:8000';
+export const API_URL = 'http://0.0.0.0:8000';
 const AuthContex = createContext<AuthProps>({});
 
 export const useAuth = () => {
